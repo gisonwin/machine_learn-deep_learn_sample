b51d444db9ad43d6639c06e75cf08665084da052
Gison Win <wenjx@linose.com>
2024-05-20 17:55:06 +0800

    code format

--- machine-learn/linear_regression/main.py
+++ machine-learn/linear_regression/main.py
@@ -1,7 +1,6 @@
 from sklearn.metrics import mean_squared_error, r2_score
 
-# 均方差 mse R方值 r2_score
-MSE = mean_squared_error(y, y_predict)
-R2 = r2_score(y, y_predict)
 
+if __name__ == '__main__':
+    pass
 
